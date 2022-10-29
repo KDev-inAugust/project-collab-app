48bb98b53399d94cd5dd530163ae3d44c9912c2b
Kane Mathis <admin@kanemathis.com>
2022-10-29 13:41:09 -0400

    client and server structure begin setup

---
+++ project-collab-app/collab-client/src/components/App.js
@@ -0,0 +1,13 @@
+
+import '../App.css';
+import TaskListContainer from './TaskListContainer';
+
+function App() {
+  return (
+    <div>
+      <TaskListContainer/>
+   </div>
+  );
+}
+
+export default App;
---
+++ project-collab-app/collab-client/src/components/TaskListContainer.js
@@ -0,0 +1,9 @@
+
+
+function TaskListContainer(){
+    return(
+        <h2>Task List Container Text</h2>
+    )
+} 
+
+export default TaskListContainer;
